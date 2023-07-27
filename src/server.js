5c055fed41e84b6cab0827ea1bd93b4c4b7c7d12
Abayomi Aluko <franicons@gmail.com>
2023-07-27 09:11:03 +0100

    feat: db-setup

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
-require('dotenv').config();
 const http = require('http');
+
+require('dotenv').config();
+const mongoose = require('mongoose');
 
 const app = require('./app');
 
@@ -9,7 +11,19 @@ const MONGO_URL = process.env.MONGO_URL
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-    console.log(`listening on PORT ${PORT}`);
+mongoose.connection.on('open', () => {
+    console.log('DB connection ready');
 });
 
+mongoose.connection.on('err', (err) => {
+    console.log(err)
+});
+
+async function startServer () {
+    await mongoose.connect(MONGO_URL);
+
+    server.listen(PORT, () => {
+        console.log(`listening on PORT ${PORT}`);
+    });
+
+}
