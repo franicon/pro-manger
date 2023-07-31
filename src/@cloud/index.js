a1b71b4deedeab9cf659814f10ad6b7d6c507f81
Abayomi Aluko <franicons@gmail.com>
2023-07-31 15:12:29 +0100

    feat:cloudinary-setup

---
+++ src/@cloud/index.js
@@ -0,0 +1,15 @@
+const {v2: cloudinary} = require("cloudinary");
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_SECRER_KEY,
+    secure: true,
+});
+
+async function attach(file) {
+    await cloudinary.uploader.upload(file, {folder: 'pro-manager'}).then(r => console.log(r) ).catch(err => console.log(err));
+
+}
+
+module.exports = attach
--- src/models/projects.model.js
+++ src/models/projects.model.js
@@ -1,19 +1,9 @@
+const path = require("path");
 const mongo = require('./projects.mongo');
-const cloudinary = require('cloudinary').v2;
-const path = require('path');
+const attach = require('../@cloud/index');
+const { generateSlug } = require('../helpers/index');
 
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_SECRER_KEY,
-    secure: true,
-});
-
-const file = path.join(__dirname, './projects.mongo.js');
-console.log(file)
-
-cloudinary.uploader.upload(file).then(r => console.log(r) );
-
+const file = path.join(__dirname, '../../src/shop-on.png');
 
 const project = {
     title: 'Building Shop-on market place',
@@ -22,40 +12,30 @@ const project = {
     priority: 'High',
     status: 'In progress',
     deadline: Date.now(),
-    attach: 'architecture.figma',
     privacy: 'Private',
     teamLead: 'Yomi Aluk'
 }
 
-const generateSlug = (title) => {
-    const slug = title.split(' ');
-    if (slug.length === 1) {
-        return slug[0].toLowerCase();
-    }
-    const getSlug = slug.slice(0, 2)
-    return getSlug.join('-').toLowerCase()
-}
-
-
-// saveNewProject(project).then(r => r);
+saveNewProject(project).then(r => r);
 
 async function saveNewProject(projects) {
+    const projectAttach = await attach(file);
     const genSlug = generateSlug(projects.title);
     const found = await findProject(projects.slug);
-
     const newProject = Object.assign(projects, {
-        slug: genSlug
+        slug: genSlug,
+        files: projectAttach
     });
 
     if (!found) {
         await mongo.create(newProject);
     } else  {
-        console.error('something went wrong')
+        console.error('something went wrong');
     }
 }
 
 async function findProject(slug) {
-    return mongo.findOne({slug})
+    return mongo.findOne({slug});
 }
 
 async function getAllProjects() {
