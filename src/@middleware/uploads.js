ccc086bc5676db04230a0aa9a1a39b6a365476fe
Abayomi Aluko <franicons@gmail.com>
2023-08-03 13:04:04 +0100

    feat:cloudinary-setup

--- src/@middleware/uploads.js
+++ src/@middleware/uploads.js
@@ -5,37 +5,24 @@ const attach = require('../@cloud/index');
 const upload = require('../@multer/index');
 
 const file  = async (req, res, next) => {
-    return upload.array('image')(req, res, () => {
-        console.log(req.files,  'files')
-        if (!req.files) return res.json({ error: 'Something went-wrong' })
+    return upload.array('image')(req, res, async () => {
+        const uploader = async (path) => await attach(path);
+        const urls = [];
+
+        const files = req.files
+        for (const file of files) {
+            const {path} = file
+
+            const newPath = await uploader(path);
+
+            urls.push(newPath);
+
+            fs.unlinkSync(path);
+        }
+        console.log(req.files, 'files')
+        if (!req.files) return res.json({error: 'Something went-wrong'})
         next()
-    })
-    // const uploader = async (path) => await attach(path);
-
-    // const urls = [];
-    //
-    // const files = req.files
-    // for (const file of files) {
-    //     const {path} = file
-    //
-    //     const newPath = await uploader(path);
-    //
-    //     urls.push(newPath);
-    //
-    //     fs.unlinkSync(path);
-    // }
-
-    // try {
-    //     console.log(files)
-    //     res.status(200).json({
-    //         message: 'Project created successfully',
-    //         data: files
-    //     });
-    // } catch (err) {
-    //     res.status(400).json({error: err});
-    // }
-
-    // next()
+    });
 }
 
 module.exports = file
